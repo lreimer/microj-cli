7336d06a8907edcbe96756931bda33a0a04f1672
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-02-06 09:23:03 +0100

    Added docs and parent command.

--- src/main/java/dev/ops/tools/microj/cmd/BuildJobCommand.java
+++ src/main/java/dev/ops/tools/microj/cmd/BuildJobCommand.java
@@ -1,15 +1,23 @@
 package dev.ops.tools.microj.cmd;
 
+import dev.ops.tools.microj.MicrojCli;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.ParentCommand;
 
 import static picocli.CommandLine.Option;
 
+/**
+ * Create and register a new build job for the current project.
+ */
 @Command(name = "buildjob", description = "Create and register a new build job")
 public class BuildJobCommand implements Runnable {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BuildJobCommand.class);
+
+    @ParentCommand
+    private MicrojCli cli;
 
     @Option(names = {"-n", "--name"}, description = "Build job name", required = true)
     private String name;
